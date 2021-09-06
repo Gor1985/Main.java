54709408cf73e24b812a154d8b20fd8b4cdb24c2
Gor1985 <87267970+Gor1985@users.noreply.github.com>
2021-09-06 15:03:33 +0300

    Main.java

---
+++ Task_1.1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "name";
+        int age = 20;
+        String city = "city";
+        System.out.println("Мое имя " + name + "мой возраст" + age + "я живу в городе" + city);
+    }
+}
