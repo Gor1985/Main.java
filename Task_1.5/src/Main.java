0f98a187ae649aee115a70ba086d45e959fb159c
Gor1985 <87267970+Gor1985@users.noreply.github.com>
2021-09-06 15:36:12 +0300

    Main.java

---
+++ Task_1.5/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int C= scanner.nextInt();
+        System.out.println((9f / 5f) * C + 32+ " */");
+    }
+}
