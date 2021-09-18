25d81136bbfa8ba085766f08833f750f9210cf1d
Gor1985 <87267970+Gor1985@users.noreply.github.com>
2021-09-18 22:26:29 +0300

    Main.java

---
+++ Task_5.1/src/Main.java
@@ -0,0 +1,39 @@
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList();
+        names.add("Анатолий");
+        names.add("Георгий");
+        names.add("Руслан");
+        names.add("Георгий");
+        names.add("Павел");
+        names.add("Руслан");
+
+            HashSet<String> strings = new HashSet<>(names);
+            System.out.println(strings);
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
