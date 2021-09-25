ee24a7172f5f52b0fcce353dd4c54fb948c8b4c6
Gor1985 <87267970+Gor1985@users.noreply.github.com>
2021-09-25 20:52:08 +0300

    Main.java

--- Chat/src/Klient/Client.java
+++ Chat/src/Klient/Client.java
@@ -7,6 +7,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -14,6 +15,7 @@ import static java.lang.System.in;
 public class Client {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
+
         try {
             Socket socket = new Socket("192.168.1.5", 8188);
             System.out.println("Успешно подключен!");
@@ -28,12 +30,13 @@ public class Client {
                 public void run() {
                     try {
 
+                    String text;
                         while (true) {
                             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                             DataInputStream in = new DataInputStream(socket.getInputStream());
                             String response = in.readUTF();
-                            String text = in.readUTF();
-                            System.out.println(response + " : " + text);
+
+                            System.out.println(response);
 
 
                         }
@@ -43,18 +46,12 @@ public class Client {
                 }
             });
             thread.start();
-                System.out.println("Введите ваше имя: ");
-                String text = scanner.nextLine();
-                out.writeUTF(text);
+            System.out.println("Введите ваше имя: ");
+            String name = scanner.nextLine();
+            out.writeUTF(name);
+            Scanner scanner1=new Scanner(System.in);
+            out.writeUTF(scanner1.nextLine());
 
-
-
-
-
-            while (true){
-                String   m=scanner.nextLine();
-                out.writeUTF(m);
-            }
 
 
         } catch (IOException exception) {
--- Chat/src/Server/Server.java
+++ Chat/src/Server/Server.java
@@ -7,46 +7,62 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.spi.CurrencyNameProvider;
 
 public class Server {
+    private static Object String;
+
     public static void main(String[] args) {
         ArrayList<Socket> usersSocket = new ArrayList<>();
+        ArrayList<String>names=new ArrayList<>();
         try {
             ServerSocket serverSocket = new ServerSocket(8188);
+
             System.out.println("Сервер запущен");
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Подключился новый клиент. ");
+
                 usersSocket.add(socket);
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 DataInputStream in = new DataInputStream(socket.getInputStream());
+
+                 DataInputStream bon=new DataInputStream(socket.getInputStream());
+                String name;
+                name=in.readUTF();
+                names.add(name);
+
+                //out.writeUTF(name);
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
 
                         try {
-                            String text;
-
+                            for (String nameSoket:names);
+                            System.out.println(name+"");
                             while (true) {
+                                String text;
                                 text = in.readUTF();
-                                System.out.println("Клиент прислал сообщение: " + text);
                                 for (Socket userSocket : usersSocket) {
-                                    DataOutputStream out = new DataOutputStream(userSocket.getOutputStream());
-                                    out.writeUTF(text);
+                                    String mex=name+" :"+text;
+                                    System.out.println(mex);
                                 }
                             }
                         } catch (IOException exception) {
                             usersSocket.remove(socket);
+                            names.remove(name);
                             System.out.println("Клиент отключился");
                         }
                     }
                 });
                 thread.start();
+
             }
 
 
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+
     }
 }
