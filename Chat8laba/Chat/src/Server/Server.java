8a8df6036a223c9943b3047a876ecb4061a30e08
Gor1985 <87267970+Gor1985@users.noreply.github.com>
2021-09-27 21:33:41 +0300

    Main.java

---
+++ Chat8laba/Chat/src/Server/Server.java
@@ -0,0 +1,113 @@
+
+package Server;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+
+public class Server {
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(8188);
+
+            System.out.println("Сервер запущен");
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                User currentUser = new User(socket, User.class.getName());
+
+
+                users.add(currentUser);
+                System.out.println("Подключился новый клиент");
+                currentUser.setOut(new DataOutputStream(currentUser.getSocket().getOutputStream()));
+                currentUser.setIn(new DataInputStream(currentUser.getSocket().getInputStream()));
+
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String name = "";
+                        String text;
+
+                        try {
+                            currentUser.getOut().writeUTF("Введите имя: ");
+                            String userName = currentUser.getIn().readUTF();
+                            for (User user : users) {
+                                name += (user.getUserName() + ", ");
+
+                                if (name.contains(userName)) {
+                                    currentUser.getOut().writeUTF("Имя занято, перезайдите");
+                                    System.out.println("Имя занято");
+                                    socket.close();
+                                }
+                            }
+
+                            if (!name.contains(userName))
+
+                                currentUser.setUserName(userName);
+                            for (User user : users)
+                                user.getOut().writeUTF("Пользователь " + currentUser.getUserName() + " присоединился к беседе");
+
+                            while (true) {
+                                text = currentUser.getIn().readUTF();
+                                if (text.equals("/onlineUsers")) {
+                                    String names = "Пользователи онлайн: ";
+                                    for (User user : users) {
+                                        names += user.getUserName() + "  ";
+
+                                    }
+                                    currentUser.getOut().writeUTF(names); // Отправили список
+                                }else if (text.indexOf("/n")==0){
+                                  String[] na= text.split("");
+                                  for (User user:users) {
+                                      if (user.getUserName().equals(na[1])) {
+                                          user.getOut().writeUTF(na[2]);
+                                          break;
+                                      }
+                                  }
+                                } else {
+                                    // Рассылка сообщения
+                                    System.out.println(currentUser.getUserName() + ": " + text);
+                                    for (User user : users) {
+                                        if (currentUser.getUuid().equals(user.getUuid())) continue;
+                                        user.getOut().writeUTF(currentUser.getUserName() + ": " + text);
+                                    }
+
+
+
+
+
+
+
+                                }
+                            }
+
+
+
+                        } catch (IOException exception) {
+                            users.remove(currentUser);
+                            for (User user : users) {
+                                try {
+                                    user.getOut().writeUTF("Пользователь " + currentUser.getUserName() + " покинул беседу");
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            System.out.println("Пользователь " + currentUser.getUserName() + " покинул беседу");
+                        }
+                    }
+                });
+                thread.start();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
+
---
+++ Chat8laba/Chat/src/Server/User.java
@@ -0,0 +1,42 @@
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.UUID;
+
+public class User<addNewUser> {
+    private Socket socket;
+    private String userName;
+    private UUID uuid;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+
+    public User(Socket socket,String userName) {
+        this.socket = socket;
+        this.uuid = UUID.randomUUID(); // Уникальный идентификатор пользователя
+      this.userName=userName;
+    }
+
+    public Socket getSocket() { return socket; }
+    public String getUserName() { return userName; }
+    public UUID getUuid() { return uuid; }
+    public DataOutputStream getOut() { return out; }
+    public DataInputStream getIn() { return in; }
+
+    public void setUserName(String userName) { this.userName = userName;
+
+
+
+   }
+
+
+    public void setOut(DataOutputStream out) { this.out = out; }
+    public void setIn(DataInputStream in) { this.in = in; }
+
+
+
+}
+
+
