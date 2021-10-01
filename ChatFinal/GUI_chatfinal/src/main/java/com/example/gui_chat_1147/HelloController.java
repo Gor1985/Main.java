78df95068a91dafa21766252667d49b3392a2091
Gor1985 <87267970+Gor1985@users.noreply.github.com>
2021-10-01 16:04:31 +0300

    Main.java

---
+++ ChatFinal/GUI_chatfinal/src/main/java/com/example/gui_chat_1147/HelloController.java
@@ -0,0 +1,81 @@
+package com.example.gui_chat_1147;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class HelloController {
+    DataOutputStream out;
+    ArrayList<String> usersName = new ArrayList<>();
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField textField;
+    @FXML
+    private TextArea onlineUsersTextArea;
+    @FXML
+    private Button connectBtn;
+    @FXML // Обработчк кнопки отправить
+    protected void handlerSend(){
+        String text = textField.getText();
+        try {
+            out.writeUTF(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        textArea.appendText(text+"\n");// Нужно добавлять текст, а не заменять
+        textField.clear(); // Очистить textField
+    }
+    @FXML
+    protected void handlerConnection(){
+        try {
+            Socket socket = new Socket("192.168.1.2", 8188);
+            // Поток ввода
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            // Поток вывода
+            out = new DataOutputStream(socket.getOutputStream());
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true){
+                            String response = "";//создаем переменную для записи сообщения
+                            Object object = ois.readObject();//чтение обьекта с сервера
+                            System.out.println(object.getClass());// узнаем класс обьекта путем метода гет класс
+                            if(object.getClass().equals(usersName.getClass())){//если класс обекта соответствует имени в списке класса
+                                usersName = (ArrayList<String>) object;
+                                System.out.println(usersName);
+                                onlineUsersTextArea.clear();
+                                for(String userName : usersName){
+                                    onlineUsersTextArea.appendText(userName+"\n");
+                                }
+                            }else{
+                                response = object.toString();
+                                textArea.appendText(response+"\n");
+                            }
+                            /*String response = in.readUTF();*/
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                        System.out.println("Соединение разорвано");
+                    }
+                }
+            });
+            thread.start();
+            connectBtn.setDisable(true);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
